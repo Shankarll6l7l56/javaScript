a8596441577982e9afee72618208a9f0f1b7c300
Shankarll6l7l56 <ucc.shankarlalkumhar@gmail.com>
2024-06-25 17:26:57 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+// number 
+
+// integerNumber
+let integerNumber = 20;
+console.log(integerNumber)
+console.log(typeof integerNumber)
+// flotNumber
+let flotNumber = 10.25;
+console.log(flotNumber)
+console.log(typeof flotNumber)
+
+// string
+// singilCode
+let singilCode = 'shankar lal kumhar'
+console.log(singilCode)
+console.log(typeof singilCode)
+// doubleCode
+let doubleCode = "shankar lal kumhar"
+console.log(doubleCode)
+console.log(typeof doubleCode)
+// boolean
+//true
+let isJavaScriptFun = true;
+console.log(isJavaScriptFun)
+console.log(typeof isJavaScriptFun)
+// false
+let isTired = false;
+console.log(isTired)
+console.log(typeof isTired)
+//undefined
+let notAssigned;
+console.log(notAssigned)
+console.log(typeof notAssigned)
+// null
+let emptyValue = null;
+console.log(emptyValue)
+console.log(typeof emptyValue)
+// bigint number
+let bigintNumber = 34562532513254698785695125365897456;
+console.log(bigintNumber)
+console.log(typeof bigintNumber)
+// object
+let person = {
+    firstName: 'shankar',
+    lastName: 'kumhar',
+    age: 23,
+    gamil: "ucc.shankarlalkumhar@gamli.com",
+};
+console.log(person)
+console.log(typeof person)
+
+
